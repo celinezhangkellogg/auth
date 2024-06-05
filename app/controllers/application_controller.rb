e210cb80ac63457d81f437b7df2bfa31604d26ba
Xinyi Zhang <celine.zhang@kellogg.northwestern.edu>
2024-06-05 08:07:47 +0000

    minor mistakes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "----- this code runs at the beginning of the request"
+    @current_user = User.find_by({"id"=>session["user_id"]})
     # find logged-in user
   end
 end
